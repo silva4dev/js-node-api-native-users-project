7dbf10755b7910b0bac7e159d072212bfb59a344
silva4dev <web4dev.lucas@outlook.com>
2023-06-05 00:53:11 -0300

    inicial

--- fundamentals/src/server.js
+++ fundamentals/src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
@@ -8,10 +9,14 @@ const server = http.createServer(async (request, response) => {
   await json(request, response);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
+    request.params = params;
+    request.query = query ? extractQueryParams(query) : {};
     return route.handler(request, response);
   }
 
